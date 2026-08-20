a508aa698672f24cf980134baade7290698dda91
Seednode <github@seedno.de>
2026-08-19 23:40:33 -0400

    Update go.mod to 1.27

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/Seednode/roulette
 
-go 1.26
+go 1.27
 
 require (
 	github.com/alecthomas/chroma/v2 v2.27.0
--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	AllowedCharacters string = `^[A-z0-9.\-_]+$`
-	ReleaseVersion    string = "13.0.2"
+	ReleaseVersion    string = "13.1.0"
 )
 
 var (
